74a9f79db781acc9ba7451d25c0bb279f02fb2a5
pawelchojnacki <chojnacki.pawel@linux.com>
2016-02-06 15:36:05 +0100

    add LibraryBook subclass

--- tutorial/intro.js
+++ tutorial/intro.js
@@ -1,3 +1,8 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 function hello(name) {
     return "Hello, " + name;
 }
@@ -9,3 +14,20 @@ var Book = (function () {
     }
     return Book;
 })();
+var LibraryBook = (function (_super) {
+    __extends(LibraryBook, _super);
+    function LibraryBook(title, isbn) {
+        _super.call(this, title, isbn);
+        this._available = true;
+    }
+    Object.defineProperty(LibraryBook.prototype, "available", {
+        get: function () { return this.available; },
+        set: function (isAvailable) {
+            console.log("'" + this.title + "' is now " + (isAvailable ? 'available' : 'unavailable'));
+            this._available = isAvailable;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return LibraryBook;
+})(Book);
--- tutorial/intro.ts
+++ tutorial/intro.ts
@@ -8,3 +8,15 @@ document.body.innerHTML = hello('world!');
 class Book {
   constructor(public title:string, public isbn:number) {}
 }
+
+class LibraryBook extends Book {
+  private _available = true;
+  constructor(title: string, isbn: number) {
+    super(title, isbn);
+  }
+  get available() {return this.available;}
+  set available(isAvailable: boolean) {
+    console.log(`'${this.title}' is now ${isAvailable ? 'available': 'unavailable'}`);
+    this._available = isAvailable;
+  }
+}
